5f228238afdd797aba17addc5b4ae3cf27a997d5
Jaro Steindorff <ja2332st-s@student.lu.se>
2023-11-28 12:59:49 +0100

    Work on the midterm assignment

--- exercises/MidTerm.R
+++ exercises/MidTerm.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(hrbrthemes)
 library(glmmTMB)
 library(dplyr)
+library(RColorBrewer)
 
 # get the data
 tephritis <- read.delim("~/BIOS4/data/tephritis.txt")
@@ -12,8 +13,7 @@ names(tephritis)
 # get an overview of the data
 # overview body length
 histo1 <- ggplot(tephritis, aes(x = BL )) +
-  geom_histogram(binwidth = 0.1, fill = "darkblue",alpha = 0.5, color = "black") +
-  theme_ipsum() +
+  geom_histogram(binwidth = 0.08, fill = "darkblue",alpha = 0.5, color = "black") +
   theme(    
     axis.line.x.bottom = element_line(color = 'black'),
     axis.line.y.left = element_line(color = 'black'),
@@ -32,6 +32,8 @@ histo1 <- ggplot(tephritis, aes(x = BL )) +
   xlab("Body length in mm")+
   ylab("frequency")
 print(histo1)
+
+ggsave("Histogram.png",histo1, width = 10, height = 10, units = "cm", dpi = 300)
 
 # density compare body length between regions
 densityplot1 <- ggplot(tephritis, aes(x=BL, fill= factor(Baltic), color= factor(Baltic)))+
@@ -89,11 +91,15 @@ print(densityplot2)
 
 # create a new factor variable that resembles all possible combination of hostplant and baltic
 tephritis$HostplantBaltic <- factor(paste(tephritis$Hostplant, tephritis$Baltic, sep = "+"))
+# create a new factor variable that resembles all possible combination of hostplant and Patry
+tephritis$HostplantPatry <- factor(paste(tephritis$Hostplant, tephritis$Patry, sep = "+"))
+# create a new factor variable that resembles all possible combination of hostplant and baltic
+tephritis$BalticPatry <- factor(paste(tephritis$Baltic, tephritis$Patry, sep = "+"))
 
 # density compare body length between different host plants and regions
 densityplot3 <- ggplot(tephritis, aes(x=BL, fill= factor(HostplantBaltic), color= factor(HostplantBaltic)))+
   geom_density(alpha=0.5, linewidth=0.2,) +
-  scale_fill_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6") ,name = "Group:", labels = c("Heterophyllum East", "Oleraceum East", "Heterophyllum West", "Oleraceum West"))+
+  scale_fill_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6") ,name = "Groups:", labels = c("Heterophyllum East", "Oleraceum East", "Heterophyllum West", "Oleraceum West"))+
   scale_color_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6") , guide = FALSE)+
   theme_ipsum()+
   theme(    
@@ -111,16 +117,18 @@ densityplot3 <- ggplot(tephritis, aes(x=BL, fill= factor(HostplantBaltic), color
     legend.title = element_text(size = 8),
     legend.text = element_text(size = 8),
     legend.key.size = unit(0.2,'cm'),
-    legend.position = c(0.9,0.9)) +
-  ggtitle("Comparison of body length of tephritis\nfrom different hostplants and regions") +
+    legend.position = c(0.92,0.9)) +
+  ggtitle("Comparison of body length of tephritis\nbetween different hostplants and regions") +
   xlab("body length in mm") +
   ylab("Density")
 print(densityplot3)
 
-# create boxplots plots for the 4 different groups
-boxplot <- ggplot(tephritis, aes(x=HostplantBaltic, y=BL, fill= factor(HostplantBaltic),))+
-  geom_boxplot(alpha=0.5, linewidth=0.2,) +
-  scale_fill_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6"), guide = FALSE)+
+# density compare body length between different host plants and patry
+densityplot4 <- ggplot(tephritis, aes(x=BL, fill= factor(HostplantPatry), color= factor(HostplantPatry)))+
+  geom_density(alpha=0.5, linewidth=0.2,) +
+  scale_fill_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6") ,
+                    name = "Groups:", labels = c("Heterophyllum Sympatry", "Oleraceum Sympatry", "Heterophyllum Allopatry", "Oleraceum Allopatry"))+
+  scale_color_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6") , guide = FALSE)+
   theme_ipsum()+
   theme(    
     panel.grid.major = element_blank(),
@@ -131,7 +139,69 @@ boxplot <- ggplot(tephritis, aes(x=HostplantBaltic, y=BL, fill= factor(Hostplant
     panel.border = element_blank(),
     plot.title = element_text(size=8),
     axis.title.x = element_text(size=8),
-    axis.text.x = element_text(size = 8, angle = 20, hjust = 1),
+    axis.text.x = element_text(size=8),
+    axis.title.y = element_text(size=8),
+    axis.text.y = element_text(size=8),
+    legend.title = element_text(size = 8),
+    legend.text = element_text(size = 8),
+    legend.key.size = unit(0.2,'cm'),
+    legend.position = c(0.92,0.9)) +
+  ggtitle("Comparison of body length of tephritis\nbetween different hostplants and patry") +
+  xlab("body length in mm") +
+  ylab("Density")
+print(densityplot4)
+
+# density compare body length between different host plants and regions
+densityplot5 <- ggplot(tephritis, aes(x=BL, fill= factor(BalticPatry), color= factor(BalticPatry)))+
+  geom_density(alpha=0.5, linewidth=0.2,) +
+  scale_fill_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6") ,
+                    name = "Groups:", labels = c("East Sympatry", "West Sympatry", "East Allopatry", "West Allopatry"))+
+  scale_color_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6") , guide = FALSE)+
+  theme_ipsum()+
+  theme(    
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line.x.bottom = element_line(color='black'),
+    axis.line.y.left = element_line(color='black'),
+    panel.border = element_blank(),
+    plot.title = element_text(size=8),
+    axis.title.x = element_text(size=8),
+    axis.text.x = element_text(size=8),
+    axis.title.y = element_text(size=8),
+    axis.text.y = element_text(size=8),
+    legend.title = element_text(size = 8),
+    legend.text = element_text(size = 8),
+    legend.key.size = unit(0.2,'cm'),
+    legend.position = c(0.92,0.9)) +
+  ggtitle("Comparison of body length of tephritis\nfrom different regions and patry") +
+  xlab("body length in mm") +
+  ylab("Density")
+print(densityplot5)
+
+ggsave("Densitiy1.png",densityplot3, width = 10, height = 10, units = "cm", dpi = 300)
+ggsave("Densitiy2.png",densityplot4, width = 10, height = 10, units = "cm", dpi = 300)
+ggsave("Densitiy3.png",densityplot5, width = 10, height = 10, units = "cm", dpi = 300)
+
+# create a new factor variable that resembles all possible combination of hostplant and baltic and Patry
+tephritis$HostplantBalticPatry <- factor(paste(tephritis$Hostplant, tephritis$Baltic, tephritis$Patry, sep = "+"))
+coul <- brewer.pal(8, "PuOr") 
+
+# create boxplots plots for the 4 different groups
+boxplot <- ggplot(tephritis, aes(x=HostplantBalticPatry, y=BL, fill= factor(HostplantBalticPatry),))+
+  geom_boxplot(alpha=0.5, linewidth=0.2,) +
+  scale_fill_manual(values = coul, guide = FALSE)+
+  theme_ipsum()+
+  theme(    
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line.x.bottom = element_line(color='black'),
+    axis.line.y.left = element_line(color='black'),
+    panel.border = element_blank(),
+    plot.title = element_text(size=8),
+    axis.title.x = element_text(size=8),
+    axis.text.x = element_text(size = 6, angle = 45, hjust = 1),
     axis.title.y = element_text(size=8),
     axis.text.y = element_text(size=8),
     legend.title = element_text(size = 8),
@@ -142,6 +212,8 @@ boxplot <- ggplot(tephritis, aes(x=HostplantBaltic, y=BL, fill= factor(Hostplant
   xlab("") +
   ylab("body length in mm")
 print(boxplot)
+
+ggsave("Boxplot.png",boxplot, width = 10, height = 10, units = "cm", dpi = 300)
 
 # create different models wit different complexity
 model1 <- lm(BL ~ Hostplant*Baltic*Sex*Patry, data = tephritis)
@@ -173,7 +245,7 @@ anova(model2)
 barplot <- ggplot(tephritis, aes(x = Patry, y = BL, fill = factor(HostplantBaltic))) +
   geom_bar(stat = "summary", fun.y = mean, position = position_dodge(), alpha = 0.5,
            color = "black") +
-  scale_fill_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6"), name = "Baltic:",
+  scale_fill_manual(values = c("#4E5166","#2CA58D","#855A5C","#2874A6"), name = "Group:",
                     labels = c("Heterophyllum East", "Oleraceum East", "Heterophyllum West", "Oleraceum West")) +
   theme_ipsum() +
   theme(
@@ -191,14 +263,15 @@ barplot <- ggplot(tephritis, aes(x = Patry, y = BL, fill = factor(HostplantBalti
     legend.title = element_text(size = 8),
     legend.text = element_text(size = 8),
     legend.key.size = unit(0.3, 'cm'),
-    legend.position = c(0.9, 0.9)
+    legend.position = c(0.9, 0.85)
   ) +
-  ggtitle("Comparison of body length of tephritis\nfrom different hostplants and regions") +
+  ggtitle("Comparison of mean body length of tephritis from different\nhostplants and regions between Allopatry and Sympatry") +
   xlab("") +
   ylab("body length in mm") +
   coord_cartesian(ylim = c(4.1, 4.8))
 
 print(barplot)
+ggsave("Bar Plot.png",barplot, width = 10, height = 10, units = "cm", dpi = 300)
 
 # Calculate mean values
 means <- tephritis %>%
@@ -227,10 +300,13 @@ interaction_plot <- ggplot(tephritis, aes(x = Patry, y = BL, color = HostplantBa
     axis.text.y = element_text(size=8),
     legend.title = element_text(size = 8),
     legend.text = element_text(size = 8),
-    legend.key.size = unit(0.5,'cm'),
-    legend.position = c(0.9,0.9)) +
-  labs(x = "Baltic Region", y = "Body Length in mm") +
-  ggtitle("Interaction Plot of Body Length\nby Baltic Region and Host Plant")
+    legend.key.size = unit(0.3,'cm'),
+    legend.position = c(0.9,0.8)) +
+  labs(x = "", y = "Body Length in mm") +
+  ggtitle("Interaction Plot of Body Length of different groups of\n Hostplants and Regions by Allopatry and Sympatry")
 
 # Display the plot
 print(interaction_plot)
+
+# save the plots
+ggsave("Interaction Plot.png",interaction_plot, width = 10, height = 10, units = "cm", dpi = 300)
