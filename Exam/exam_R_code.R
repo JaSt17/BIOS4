a3f569bf2aa4002a2d9f42ec7f3063d4aef7d603
Jaro Steindorff <ja2332st-s@student.lu.se>
2024-01-02 11:31:59 +0100

    changed the research question and added the model for native plants

--- Exam/exam_R_code.R
+++ Exam/exam_R_code.R
@@ -1,7 +1,11 @@
 #load packages
 library(ggplot2)
-library(MASS)
+library(hrbrthemes)
 library(glmmTMB)
+library(dplyr)
+library(RColorBrewer)
+library(lme4)
+library(apaTables)
 
 #load the data
 data <- read.csv("~/BIOS4/Exam/exam2023_data.csv")
@@ -14,20 +18,16 @@ data <- na.omit(data)
 #landscape position is a factor
 data$landscape_position <- factor(data$landscape_position)
 
-# new colum with the percentage cover of exotic plants
-data$ExoticPlant_cover = data$ExoticPerennialHerb_cover +
-  data$ExoticPerennialGrass_cover + 
+# new column with the percentage cover of exotic plants
+data$ExoticPlant_cover <- data$ExoticPerennialHerb_cover +
+  data$ExoticPerennialGrass_cover +
   data$ExoticAnnualHerb_cover +
-  data$ExoticAnnualGrass_cover +
-  data$ExoticShrub_cover
+  data$ExoticAnnualGrass_cover
 
-# new colum with the percentage cover of native plants
+# new column with the percentage cover of native plants
 data$NativePlant_cover <- data$NativePerennialHerb_cover + 
-  data$NativePerennialGrass_cover + 
-  data$NativePerennialFern_cover + 
-  data$NativePerennialGraminoid_cover +
-  data$NativeShrub_cover 
-
+  data$NativePerennialGrass_cover 
+  
 # histogram of Exotic Plant cover
 histo1 <- ggplot(data) +
   geom_histogram(aes(x = ExoticPlant_cover ), binwidth = 2, fill = "#B99470",alpha = 0.5, color = "black") +
@@ -87,7 +87,8 @@ plot(histo2)
 # start fitting models with the most important abiotic factors
 # start with a model including all abiotic factors and then remove the least important ones
 
-# test different complex models
+# start with exotic plant cover
+# test different complex models 
 model1 <- glmmTMB(data = data, ExoticPlant_cover~annual_precipitation+ 
                 precipitation_warmest_quarter + precipitation_coldest_quarter +
                 K_perc + Th_ppm + U_ppm + SRad_Jan + SRad_Jul +
@@ -110,10 +111,11 @@ lh <- exp(-0.5*AICTab$delta)
 AICTab$w <- round(lh/sum(lh), 2)
 AICTab
 
-# model 2 is the best model now we reduce the number of abiotic factors to make the model simpler
+# model 1 (mixed linear model) is the model which fits best
+# now we reduce the number of abiotic factors to make the model simpler
 m1 <- lm(ExoticPlant_cover~ Landscape.position + annual_precipitation+ 
-               precipitation_warmest_quarter + precipitation_coldest_quarter +
-               K_perc + Th_ppm + U_ppm + SRad_Jan + SRad_Jul, data=data)
+           precipitation_warmest_quarter + precipitation_coldest_quarter +
+           K_perc + Th_ppm + U_ppm + SRad_Jan + SRad_Jul, data=data)
 m2 <- lm(ExoticPlant_cover~ Landscape.position + annual_precipitation+ 
            precipitation_warmest_quarter + precipitation_coldest_quarter +
            K_perc + U_ppm + SRad_Jan + SRad_Jul, data=data)
@@ -121,8 +123,8 @@ m3 <- lm(ExoticPlant_cover~ Landscape.position + annual_precipitation+
            precipitation_warmest_quarter + precipitation_coldest_quarter +
            K_perc + U_ppm, data=data)
 m4<- lm(ExoticPlant_cover~ annual_precipitation+ 
-           precipitation_warmest_quarter + precipitation_coldest_quarter +
-           K_perc + U_ppm, data=data)
+          precipitation_warmest_quarter + precipitation_coldest_quarter +
+          K_perc + U_ppm, data=data)
 m5<- lm(ExoticPlant_cover~ annual_precipitation+ 
           precipitation_coldest_quarter +
           K_perc + U_ppm, data=data)
@@ -139,4 +141,108 @@ lh <- exp(-0.5*AICTab$delta)
 AICTab$w <- round(lh/sum(lh), 2)
 AICTab
 
- 
+# show the summary of the best fitting model 
+summary(m3)
+anova(m3)
+# calculate the effect size of the different abiotic factors
+effectsize_native <- eta_squared(anova(m3), partial = TRUE)
+
+# now repeat the steps for native plant cover
+# test different complex models
+model1 <- glmmTMB(data = data, NativePlant_cover~annual_precipitation+ 
+                     precipitation_warmest_quarter + precipitation_coldest_quarter +
+                     K_perc + Th_ppm + U_ppm + SRad_Jan + SRad_Jul +
+                     (1 | Landscape.position))
+model2 <- glmmTMB(data = data, NativePlant_cover~annual_precipitation+ 
+                     precipitation_warmest_quarter + precipitation_coldest_quarter +
+                     K_perc + Th_ppm + U_ppm + SRad_Jan + SRad_Jul +
+                     (1 | Landscape.position), family="poisson")
+model3 <- lm(NativePlant_cover~ Landscape.position + annual_precipitation+
+                precipitation_warmest_quarter + precipitation_coldest_quarter +
+                K_perc + Th_ppm + U_ppm + SRad_Jan + SRad_Jul, data=data)
+
+# evaluate the different models
+mlist <- list(model1, model2, model3)
+AICTab <- AIC(model1, model2, model3)
+AICTab$logLik <- unlist(lapply(mlist, logLik))
+AICTab <- AICTab[order(AICTab$AIC, decreasing=F),]
+AICTab$delta <- round(AICTab$AIC - min(AICTab$AIC), 2)
+lh <- exp(-0.5*AICTab$delta)
+AICTab$w <- round(lh/sum(lh), 2)
+AICTab
+
+# model 3 (the simple linear model) is the model which fits best
+# now we reduce the number of abiotic factors to make the model simpler
+m1 <- lm(NativePlant_cover~ Landscape.position + annual_precipitation+ 
+           precipitation_warmest_quarter + precipitation_coldest_quarter +
+           K_perc + Th_ppm + U_ppm + SRad_Jan + SRad_Jul, data=data)
+m2 <- lm(NativePlant_cover~ Landscape.position + annual_precipitation+ 
+           precipitation_warmest_quarter + precipitation_coldest_quarter +
+           K_perc + U_ppm + SRad_Jan + SRad_Jul, data=data)
+m3 <- lm(NativePlant_cover~ Landscape.position + annual_precipitation+ 
+           precipitation_warmest_quarter + precipitation_coldest_quarter +
+           K_perc + U_ppm, data=data)
+m4<- lm(NativePlant_cover~ annual_precipitation+ 
+          precipitation_warmest_quarter + precipitation_coldest_quarter +
+          K_perc + U_ppm, data=data)
+m5<- lm(NativePlant_cover~ annual_precipitation+ 
+          precipitation_coldest_quarter +
+          K_perc + U_ppm, data=data)
+m6<- lm(NativePlant_cover~ annual_precipitation+ 
+          precipitation_coldest_quarter + K_perc, data=data)
+m7<- lm(NativePlant_cover~ 1, data=data)
+# evaluate the different models
+mlist <- list(m1, m2, m3, m4, m5, m6, m7)
+AICTab <- AIC(m1, m2, m3, m4, m5, m6, m7)
+AICTab$logLik <- unlist(lapply(mlist, logLik))
+AICTab <- AICTab[order(AICTab$AIC, decreasing=F),]
+AICTab$delta <- round(AICTab$AIC - min(AICTab$AIC), 2)
+lh <- exp(-0.5*AICTab$delta)
+AICTab$w <- round(lh/sum(lh), 2)
+AICTab
+
+# show the summary of the best fitting model 
+summary(m3)
+anova(m3)
+# calculate the effect size of the different abiotic factors
+effectsize_native <- eta_squared(anova(m3), partial = TRUE)
+
+#create a dataframe which has the Parameter for both native and exotic plant cover
+effectsize <- data.frame(Parameter = rep(c("Landscape",
+                                       "annual precipitation",
+                                       "precipitation (warmest quarter)",
+                                       "precipitation (coldest quarter)",
+                                       "K concentration in %",
+                                       "U concentration in ppm"), each = 2),
+                         Value = c(effectsize_native$Eta2_partial[1], effectsize_exotic$Eta2_partial[1],
+                                   effectsize_native$Eta2_partial[2], effectsize_exotic$Eta2_partial[2],
+                                   effectsize_native$Eta2_partial[3], effectsize_exotic$Eta2_partial[3],
+                                   effectsize_native$Eta2_partial[4], effectsize_exotic$Eta2_partial[4],
+                                   effectsize_native$Eta2_partial[5], effectsize_exotic$Eta2_partial[5],
+                                   effectsize_native$Eta2_partial[6], effectsize_exotic$Eta2_partial[6]),
+                         Group = rep(c("native", "exotic"), each = 1))
+# plot the effect size of the different abiotic factors on native and exotic plant cover
+# scatterplot of exotic plant cover vs. native plant cover
+bar1 <- ggplot(effectsize, aes(x = Parameter, y = Value, fill = Group)) +
+  geom_bar(stat = "identity", position = "dodge", color = "black") +
+  scale_fill_manual(values = c("#B99470", "#5F7F52")) +
+  theme(    
+    axis.line.x.bottom = element_line(color = 'black'),
+    axis.line.y.left = element_line(color = 'black'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    plot.title = element_text(size = 8),
+    axis.title.x = element_text(size = 8),
+    axis.text.x = element_text(size = 8, angle = 45, hjust = 1),
+    axis.title.y = element_text(size = 8),
+    axis.text.y = element_text(size = 8),
+    legend.text = element_text(size = 8),
+    legend.title = element_text(size = 8),
+    legend.position = c(0.9, 0.8),
+    legend.key.size = unit(0.5, 'cm')) +
+  ggtitle("Effect sizes of abiotic factors on the observation of exotic and native plants")+
+  xlab("")+
+  ylab("effect size")
+plot(bar1)
+
+
